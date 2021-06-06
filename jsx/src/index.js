b0f7dd8cb62f08501535f211f0926b43ce7cb824
KyleWalkerley <walkerleykyle@gmail.com>
2021-06-06 23:11:25 +0200

    Section2 Vid22

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -1,3 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
 
+
+const App = () => {
+    const buttonText = { text: 'Click me'};
+    const labelText = 'Enter name here: ';
+
+
+    return( 
+        <div>
+            <label className ="label" for="name">
+                {labelText}
+            </label>
+            <input id="name" type="text" />
+            <button style={{backgroundColor: 'blue', color: 'white'}}>
+                {buttonText.text}
+            </button>
+        </div>
+    );
+};
+
+ReactDOM.render(
+    <App />,
+    document.querySelector('#root')
+);
+
+
