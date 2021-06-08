50ba2625ddb3509b5300bdb02007b7c3d1f4b2d2
KyleWalkerley <walkerleykyle@gmail.com>
2021-06-08 22:40:48 +0200

    8 June Commit

--- components/src/index.js
+++ components/src/index.js
@@ -1,12 +1,40 @@
 import React from 'react';
-import ReactDOM from 'react-dom'
+import ReactDom from 'react-dom';
+import faker from 'faker';
+import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
 
-const App =() => {
-    return <div>Hi there!</div>
+const App = () => {
+    return (      
+        <div className="ui container comments">
+            <ApprovalCard>
+                <CommentDetail 
+                    author="Sam" 
+                    timeAgo="Today at 4:45PM" 
+                    commentPost="Nice Blog post!"
+                    avatar={faker.image.avatar()}
+                />
+            </ApprovalCard>
+
+            <ApprovalCard>
+                <CommentDetail 
+                    author="Alex" 
+                    timeAgo="Today at 2:00PM" 
+                    commentPost="Love the blog of the day post!"
+                    avatar={faker.image.avatar()} 
+                />
+            </ApprovalCard>
+            
+            <ApprovalCard>  
+                <CommentDetail 
+                    author="Jane" 
+                    timeAgo="Yesterday at 5:00PM" 
+                    commentPost="Nice blog!"
+                    avatar={faker.image.avatar()} 
+                />
+            </ApprovalCard>
+        </div>
+    );
 };
 
-ReactDOM.render(
-    <App />,
-    document.querySelector('#root')
-);
-
+ReactDom.render(<App />, document.querySelector('#root'));
