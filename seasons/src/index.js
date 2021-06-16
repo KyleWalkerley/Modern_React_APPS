8be5c753c291ae179ddf70812f86285eab57f17e
KyleWalkerley <walkerleykyle@gmail.com>
2021-06-16 23:06:01 +0200

    16 June commit

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,10 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import SeasonDisplay from "./SeasonDisplay";
+import Spinner from "./Spinner";
 
-if (module.hot) {
-  module.hot.accept();
-}
 
 class App extends React.Component {
   state = { lat: null, errorMessage: "" };
@@ -16,8 +14,7 @@ class App extends React.Component {
     );
   }
 
-  // React says we have to define render!!
-  render() {
+  renderContent() {
     if (this.state.errorMessage && !this.state.lat) {
       return <div>Error: {this.state.errorMessage}</div>;
     }
@@ -26,7 +23,11 @@ class App extends React.Component {
       return <SeasonDisplay lat={this.state.lat} />;
     }
 
-    return <div>Loading!</div>;
+    return <Spinner message="Please accept location request" />;
+  }
+
+  render() {
+    return <div className="border red">{this.renderContent()}</div>;
   }
 }
 
